1c7e2221f96b0ff1b5bdae5a561f9251871de8ee
Himank_garg <himank.garg04@gmail.com>
2026-09-19 18:35:22 +0530

    test-10

--- scripts/review.py
+++ scripts/review.py
@@ -1,6 +1,8 @@
 import subprocess
 from google import genai
 import os
+import smtplib
+from email.message import EmailMessage
 
 
 def getDiff():
@@ -11,6 +13,21 @@ def getDiff():
 client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
+def send_email(html_content):
+    msg = EmailMessage()
+    msg['Subject'] = 'Code Review Feedback'
+    msg['From'] = "himank.garg04@gmail.com"
+    msg['To'] = "himank.garg04@gmail.com"
+    msg.set_content("Please find the code review feedback below.")
+    msg.set_content(html_content, subtype='html')
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login("himank.garg04@gmail.com", os.getenv("MAIL_APP_PASSWORD"))
+        smtp.send_message(msg)
+
+    return "Email sent successfully!"
+
+
 def main():
     diff = getDiff()
     prompt = f"Please review the following code changes and provide feedback:\n\n Mandatory: provide the output in html that can use to send in mail\n\n{diff}"
@@ -18,7 +35,9 @@ def main():
         model = "gemini-3.6-flash",
         contents = prompt
     )
-    print("Code Review Feedback:")
-    print(response.text)
+    
+    html = response.text
+    send_email(html)
+    
 
 main()
