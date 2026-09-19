e0cf41f9d4bf526794adef81dfb90c885ca1236c
Himank_garg <himank.garg04@gmail.com>
2026-09-19 17:53:18 +0530

    test-4

--- scripts/review.py
+++ scripts/review.py
@@ -2,7 +2,7 @@ import subprocess
 
 
 def getDiff():
-    diff =subprocess.check_output(["git", "diff"], text=True)
+    diff =subprocess.check_output(["git", "show"], text=True)
     return diff
 
 
