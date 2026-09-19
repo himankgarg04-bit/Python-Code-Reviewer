77074980932ddb6e2175040f8bc34ff0f01f452f
Himank_garg <himank.garg04@gmail.com>
2026-09-19 18:18:28 +0530

    test-8

--- scripts/review.py
+++ scripts/review.py
@@ -14,9 +14,9 @@ client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
 def main():
     diff = getDiff()
     prompt = f"Please review the following code changes and provide feedback:\n\n{diff}"
-    response = client.models.generate_contents(
-        model = "gemini-3-flash-preview",
-        content = prompt
+    response = client.models.generate_content(
+        model = "gemini-3.6-flash",
+        contents = prompt
     )
     print("Code Review Feedback:")
     print(response.text)
